4640eb07fc6b7441eae41d8b6d91f592bc99f370
GustavoDeOliveira <6ustavo.0liveira@gmail.com>
2022-03-27 20:38:05 -0300

    Adicionando domínio pessoa

---
+++ apijogo/src/main/java/br/edu/ifrs/riogrande/tads/apijogo/app/services/dto/PessoaRequest.java
@@ -0,0 +1,29 @@
+package br.edu.ifrs.riogrande.tads.apijogo.app.services.dto;
+
+public class PessoaRequest {
+
+	private String cpf; // chave
+	private String nome;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa " + cpf + " " + nome;
+	}
+
+}
