52bd49a04471ac602b98a0a978d2c0326f8221dd
GustavoDeOliveira <6ustavo.0liveira@gmail.com>
2022-03-27 19:24:01 -0300

    Inicializando projeto SpringBoot

---
+++ apijogo/src/main/java/br/edu/ifrs/riogrande/tads/apijogo/ApijogoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.riogrande.tads.apijogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApijogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApijogoApplication.class, args);
+	}
+
+}
---
+++ apijogo/src/test/java/br/edu/ifrs/riogrande/tads/apijogo/ApijogoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.riogrande.tads.apijogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApijogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
