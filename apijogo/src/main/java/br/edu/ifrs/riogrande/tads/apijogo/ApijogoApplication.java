aa11f4324fa9b255744051fddc5e2a552c3b09d5
GustavoDeOliveira <6ustavo.0liveira@gmail.com>
2022-03-27 19:24:22 -0300

    Corrigindo pom

--- apijogo/src/main/java/br/edu/ifrs/riogrande/tads/apijogo/ApijogoApplication.java
+++ apijogo/src/main/java/br/edu/ifrs/riogrande/tads/apijogo/ApijogoApplication.java
@@ -3,6 +3,7 @@ package br.edu.ifrs.riogrande.tads.apijogo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApijogoApplication {
 
